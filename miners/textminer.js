ca318b6ffe654e0645b0ba791866c05d91b2d32d
teckthor <ademarjunior@unigranrio.br>
2019-04-23 15:31:06 -0300

    Adcionando funcao de resumo de texto nas lingas pt e en

--- miners/textminer.js
+++ miners/textminer.js
@@ -7,14 +7,18 @@ async function scrapTextFromWikipedia(content) {
     //console.log("Recebi os termos de busca: " + content.serchTerm + " E a lingua " + content.languageOfserch)
 
     await fetchContentFromWikipidia(content)
-    sanitizeContent(content)
+        sanitizeContent(content)
+    await summarizerContent(content)
     // breakContentIntoSentences(content)
 
     async function fetchContentFromWikipidia(content) {
 
         const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.serchTerm)
+        const wikipediaResponse = await wikipediaAlgorithm.pipe({
+            "lang" : content.languageOfserch,
+            "articleName" : content.serchTerm
+        })
         const wikipediaContent = wikipediaResponse.get()
 
         content.sourceContentOriginal = wikipediaContent.content
@@ -42,6 +46,20 @@ async function scrapTextFromWikipedia(content) {
 
     function removeDatesInParentheses(text) {
         return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
+    }
+
+    async function summarizerContent (content) {
+    
+    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+    const algorithmSummarizer = algorithmiaAuthenticated.algo('nlp/Summarizer/0.1.8') 
+    const textResponseSumarizer = await algorithmSummarizer.pipe(content.sourceContentSanitized)
+        
+    const textSumarize = textResponseSumarizer.get()
+        content.sourceContentSumarize = textSumarize
+    
+    // .then(function(output) {
+    //     console.log(output);
+    // });
     }
 }
 
